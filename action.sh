cb8155d6aa38b994e775349bc160a555aab61528
bloodearnest <simon.davy@thedatalab.org>
2022-02-22 13:03:37 +0000

    add concurrency and time limits

--- action.sh
+++ action.sh
@@ -1,29 +1,37 @@
 #!/bin/bash
 set -euo pipefail
 
+project_dir=${1:-}
+
 # Repeat the character 80 times
 long_line=$(printf '―%.0s' {1..80})
 bold=$(echo -e "\033[1m")
 reset=$(echo -e "\033[0m")
 
-echo "$long_line"
-echo "${bold}→ Preparing${reset}"
-echo
-echo "Checking out commit"
-echo "https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
-echo
-
-if [[ -z "$GITHUB_TOKEN" ]]; then
-  auth_header=''
+if test -n "$project_dir"; then
+    echo "$long_line"
+    ehco "switching to $project_dir"
+    cd "$project_dir"
 else
-  auth_header="authorization: Bearer $GITHUB_TOKEN"
+    echo "$long_line"
+    echo "${bold}→ Preparing${reset}"
+    echo
+    echo "Checking out commit"
+    echo "https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
+    echo
+
+    if [[ -z "$GITHUB_TOKEN" ]]; then
+    auth_header=''
+    else
+    auth_header="authorization: Bearer $GITHUB_TOKEN"
+    fi
+    curl \
+    --silent \
+    --header "$auth_header" \
+    -L "https://api.github.com/repos/$GITHUB_REPOSITORY/tarball/$GITHUB_SHA" \
+    | \
+    tar --strip-components=1 -xzf -
 fi
-curl \
-  --silent \
-  --header "$auth_header" \
-  -L https://api.github.com/repos/$GITHUB_REPOSITORY/tarball/$GITHUB_SHA \
-  | \
-  tar --strip-components=1 -xzf -
 
 python3 -m pip install --quiet --quiet --upgrade opensafely
 echo "Installing $(opensafely --version)"
